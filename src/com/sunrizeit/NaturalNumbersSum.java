d433a4c430b0568f3bccc9fdefc7080139916a91
CHANDUMCA <chandumca234@gmail.com>
2025-08-24 23:14:24 +0530

    Program to find the sum of natural numbers up to the given input

---
+++ src/com/sunrizeit/NaturalNumbersSum.java
@@ -0,0 +1,37 @@
+package com.sunrizeit;
+
+import java.util.Scanner;
+
+// Program to find the sum of the first n natural numbers
+public class NaturalNumbersSum {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("** Program to find the sum of natural numbers up to the given input **");
+			findSum(sc); // Start the sum calculation process
+		}
+	}
+
+	private static void findSum(Scanner sc) {
+		System.out.print("Enter N value to find natural numbers: ");
+		int n = sc.nextInt(); // Read the value of n
+
+		// Correct formula for the sum of the first n natural numbers
+		int sum = n * (n + 1) / 2;
+		System.out.println("Sum of natural numbers up to the given number is " + sum);
+
+		// Ask the user if they want to try again
+		System.out.println("Do you want to try one more? If yes, enter Y else N");
+		String confirm = sc.next();
+
+		// Handle user response
+		if (confirm.equalsIgnoreCase("Y")) {
+			findSum(sc); // Call findSum again for a new input
+		} else if (confirm.equalsIgnoreCase("N")) {
+			System.out.println("Thank you for using the program!");
+		} else {
+			System.out.println("Please select Y or N");
+			findSum(sc); // Prompt again for valid input
+		}
+	}
+}
